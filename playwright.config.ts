f00ca43b6ccf4f91e91850f9f6147b30e5e98b33
Daniel Gil <danielgilortiz@icloud.com>
2025-05-18 20:09:51 +0200

    config: set testIdAttribute to data-testid

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    // testIdAttribute: "data-test"
+    testIdAttribute: "id"
   },
 
   /* Configure projects for major browsers */
