870c3ee47078b235bee89bae78cd418c51645b94
Daniel Gil <danielgilortiz@icloud.com>
2025-05-19 22:32:05 +0200

    chore: retry failing tests 1 time

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Retry failing tests 1 time */
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://parabank.parasoft.com',
 
-    /* Collect trace only for failed tests */
+    /* Collect trace when retrying the failed test and on failure */
     trace: 'retain-on-failure',
     testIdAttribute: "id"
   },
